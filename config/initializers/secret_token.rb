478ecdad550a8e7915312201462bf137b6b4494f
Tee Parham <tee@neighborland.com>
2013-06-01 14:47:24 -0600

    generate initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhonelyFeud::Application.config.secret_key_base = '65ebb96bf803d88e9772120d929ffe0a636c2ad42518ba8f4a8c16349aac1a86361db887cea47bb0f63844b8d51262d5e2549113db48fc52bbb9a0b2355eeaeb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhonelyFeud::Application.config.session_store :cookie_store, key: '_phonely_feud_session'
