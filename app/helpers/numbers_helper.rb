3a5ab9c8dd1050e52f17aa7ebdb3ec28545b53ed
Tee Parham <tee@neighborland.com>
2013-06-03 07:44:45 -0600

    nice up forms & buttons

--- app/helpers/numbers_helper.rb
+++ app/helpers/numbers_helper.rb
@@ -2,7 +2,7 @@ module NumbersHelper
 
   def vote_label(vote)
     if vote == 1
-      raw %Q{<span class="round alert label">Scam</span>}
+      raw %Q{<span class="alert label">Scam</span>}
     else
       raw %Q{<span class="success label">OK</span>}
     end
